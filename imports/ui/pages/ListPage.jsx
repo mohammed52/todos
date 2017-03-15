f1fcd9c7867c410dcf4f524765b0a6da0a0a8628
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2017-03-15 16:58:19 +0500

    redux added

--- imports/ui/pages/ListPage.jsx
+++ imports/ui/pages/ListPage.jsx
@@ -106,11 +106,11 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     onBtnClickChangeColor: () => {
       function getRandomColor() {
         var letters = '0123456789ABCDEF';
-        var color = '#';
+        var color1 = '#';
         for (var i = 0; i < 6; i++ ) {
-            color += letters[Math.floor(Math.random() * 16)];
+            color1 += letters[Math.floor(Math.random() * 16)];
         }
-        return color;
+        return color1;
       }
 
       dispatch(changeBackgroundColor(getRandomColor()))
